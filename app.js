3a0fe8a110979590763e9b62f596b33ec3d4566c
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-07-07 16:08:22 +0400

    map method under hood

--- app.js
+++ app.js
@@ -14,4 +14,18 @@
 // })
 
 
+// Array map method under hood
+// Array.prototype.myMap = function(callback) {
+//  let mapedArray = [];
+//  for (let i = 0; i < this.length; i++) {
+//    let mapedItems =  callback(this[i], i, this);
+//    mapedArray.push(mapedItems)
+//  }
+//  return mapedArray
+// }
+
+// let array = [1,2,3];
+// let maped = array.myMap(el => el  * 10)
+
+
 
