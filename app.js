375b2402463ec55297eeb74e69875df52d986269
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-07-07 16:06:02 +0400

    add foreach method under hood

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // Array methods under hood
+
+
+// Array foreach method under hood
+// Array.prototype.myForEach = function(callback) {
+//  for (let i = 0; i < this.length; i++) {
+//     callback(this[i], i, this)
+//  }
+// }
+
+// let array = [1,2,3];
+// array.myForEach((el,i,array) => {
+//     return array[i] = el * 15
+// })
+
+
+
